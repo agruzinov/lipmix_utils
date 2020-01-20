341759a6b8a0e23693d025efb7a04295032a4be6
Andrey Gruzinov <agruzinov@embl-hamburg.de>
2020-01-20 19:26:25 +0100

    New. Added script for cluster run using SLURM. Added example run of this script in /cluster_run_example directory.

---
+++ lipmix_utils/cluster_run_example/test_lipmix_MLV_10_total/test_lipmix_MLV_n_dist_1_[1_2_3_4_7]_101/test_lipmix_MLV_n_dist_1_[1_2_3_4_7]_101_clust.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#SBATCH --export=ALL
+#SBATCH --error=%x.stderrn#SBATCH --output=%x.stdout
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=agruzinov@embl-hamburg.de
+source /apps/prod/atsas/atsas-env latest
+cd ${SLURM_SUBMIT_DIR}
+../../lipmix <test_lipmix_MLV_n_dist_1_[1_2_3_4_7]_101
---
+++ lipmix_utils/cluster_run_example/test_lipmix_MLV_10_total/test_lipmix_MLV_n_dist_1_[1_3_4_6_7_8_9_10]_1001/test_lipmix_MLV_n_dist_1_[1_3_4_6_7_8_9_10]_1001_clust.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#SBATCH --export=ALL
+#SBATCH --error=%x.stderrn#SBATCH --output=%x.stdout
+#SBATCH --mail-type=END
+#SBATCH --mail-user=agruzinov@embl-hamburg.de
+source /apps/prod/atsas/atsas-env latest
+cd ${SLURM_SUBMIT_DIR}
+../../lipmix <test_lipmix_MLV_n_dist_1_[1_3_4_6_7_8_9_10]_1001
