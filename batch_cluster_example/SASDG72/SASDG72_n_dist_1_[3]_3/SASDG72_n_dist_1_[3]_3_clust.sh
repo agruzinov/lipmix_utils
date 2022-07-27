14f1be1c93a8a93cba3958dbfa7801575ea387f1
agruzinov <78498907+agruzinov@users.noreply.github.com>
2022-07-28 00:10:51 +0200

    Added cluster batch example. Tested on ATSAS 3.0.4.

---
+++ batch_cluster_example/SASDG72/SASDG72_n_dist_1_[3]_3/SASDG72_n_dist_1_[3]_3_clust.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#SBATCH --partition=ps
+#SBATCH --error=%x.stderrn#SBATCH --output=%x.stdout
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=andrey.gruzinov@desy.de
+cd ${SLURM_SUBMIT_DIR}
+/software/atsas/3.0.4/bin/lipmix < SASDG72_n_dist_1_[3]_3
