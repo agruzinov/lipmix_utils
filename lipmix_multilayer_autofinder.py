b96594e5359f4f914bc49e3cd946ea641c1c0024
agruzinov <78498907+agruzinov@users.noreply.github.com>
2022-07-27 20:25:45 +0200

    Cleanup

--- lipmix_multilayer_autofinder.py
+++ lipmix_multilayer_autofinder.py
@@ -6,6 +6,11 @@ import argparse
 import shutil
 import re
 
+def natural_sort(l): 
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key=alphanum_key)
+
 
 def generate_lipmix_submission(l,d, dat):
 
