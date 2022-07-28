bb3aa58bb14c058b3e2e782ab73bfff0e0c68c79
agruzinov <78498907+agruzinov@users.noreply.github.com>
2022-07-28 14:22:26 +0200

    Code cleanup

--- lipmix_multilayer_autofinder.py
+++ lipmix_multilayer_autofinder.py
@@ -6,14 +6,14 @@ import argparse
 import shutil
 import re
 
-def natural_sort(l): 
+
+def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
-    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    alphanum_key = lambda key: [convert(c) for c in re.split('(\d+)', key)]
     return sorted(l, key=alphanum_key)
 
 
-def generate_lipmix_submission(l,d, dat):
-
+def generate_lipmix_submission(l, d, dat):
     # l - number of bilayers
     # d - number of distributions
 
@@ -25,40 +25,39 @@ def generate_lipmix_submission(l,d, dat):
 
     total_l = l
 
-
     os.chdir(directory)
-    
-    for dis in np.arange(1,n_dist+1):
 
-        list_l = list(np.arange(1,total_l+1))
+    for dis in np.arange(1, n_dist + 1):
+
+        list_l = list(np.arange(1, total_l + 1))
 
         tot_com = 0
 
-        for l in np.arange(1,total_l+1):
+        for l in np.arange(1, total_l + 1):
 
             perm = itertools.combinations(list_l, l)
 
             for p in perm:
-                tot_com = tot_com+1
-                fn = dat.split(".dat")[0]+"_n_dist_"+str(dis)+"_"+str(list(p)).replace(", ","_")+"_"+str(tot_com)
+                tot_com = tot_com + 1
+                fn = dat.split(".dat")[0] + "_n_dist_" + str(dis) + "_" + str(list(p)).replace(", ", "_") + "_" + str(
+                    tot_com)
 
                 directory = fn
                 if not os.path.exists(directory):
                     os.makedirs(directory)
 
                 os.chdir(directory)
-                shutil.copy2("../../"+dat, "../"+directory)
+                shutil.copy2("../../" + dat, "../" + directory)
 
-                f=open(fn,"w")
+                f = open(fn, "w")
 
-                
                 f.write("i\n")
                 f.write("!!!!!!!!!!!!!!!\n")
                 f.write("pro us\n")
                 f.write("Series titles\n")
                 f.write("Partial data titles\n")
                 f.write("EXPERIMENT\n")
-                f.write(str(dis+2)+"           !! Number of components\n")
+                f.write(str(dis + 2) + "           !! Number of components\n")
                 f.write("0.5000   !! System concentration\n")
                 f.write("DIFFUSE                         !! Type of the DIFFUSE Phase (SFF approximation)\n")
                 f.write("0.50    0.0000    100.000       !! Volume fraction of the bilayer component\n")
@@ -69,16 +68,19 @@ def generate_lipmix_submission(l,d, dat):
                 f.write("0.172   0.000     1.780         !! Amplitude ratio (Peak2/Peak1) (positive density)\n")
                 f.write("0.196   0.080     0.450         !! Width of peak3 position (negative density)\n")
                 f.write("0.884   0.270     3.890         !! Amplitude ratio (Peak3/Peak1) (negative density)\n")
-                f.write("0.01    0.01      0.020         !! Callie parameter (measure for the bilayer bending fluctuations)\n")
-    
-                
+                f.write(
+                    "0.01    0.01      0.020         !! Callie parameter (measure for the bilayer bending "
+                    "fluctuations)\n")
 
-                f.write("   "+str(len(p))+"        "+str(len(p))+"       "+str(len(p))+"       "+"!! Total Number of multilayer vesicles (alwayed FIXED)\n")
+                f.write("   " + str(len(p)) + "        " + str(len(p)) + "       " + str(
+                    len(p)) + "       " + "!! Total Number of multilayer vesicles (always FIXED)\n")
                 for pi in p:
-                    f.write("   "+str(pi)+"       "+str(pi)+"       "+str(pi)+"       "+"!! Number of layers for vesicle type"+str(pi)+"\n")
-                    print (f.write("   1.0      0.0       1.0       !! Weight contribution of vesicle type "+str(pi)+"\n"))
+                    f.write("   " + str(pi) + "       " + str(pi) + "       " + str(
+                        pi) + "       " + "!! Number of layers for vesicle type" + str(pi) + "\n")
+                    print(f.write(
+                        "   1.0      0.0       1.0       !! Weight contribution of vesicle type " + str(pi) + "\n"))
 
-                for dis in np.arange(1,dis+1):
+                for dis in np.arange(1, dis + 1):
                     f.write("SPHERE                          !! Type of the SPHERE Phase (SFF approximation)\n")
                     f.write("0.5000    0.0000    100.000     !! Volume fraction of the component (vesicle/micelle)\n")
                     f.write("0.0000    0.0000    0.0000      !! Inner (core) radius of the sphere\n")
@@ -89,8 +91,6 @@ def generate_lipmix_submission(l,d, dat):
                     f.write("400.000   400.000   400.000     !! Hard-sphere radius (for interactions only)\n")
                     f.write("2                               !! Schulz distribution 2 (Gauss distribution 1)\n")
                     f.write("0.0000    0.0000    0.0000      !! stickiness parameter (for interactions only)\n")
-
-
 
                 f.write("SPHERE\n")
                 f.write("   0.0050    0.0000    1.0000    !! Volume fraction of the component (vesicle/micelle)\n")
@@ -103,8 +103,8 @@ def generate_lipmix_submission(l,d, dat):
                 f.write("2                                !! Schulz distribution 2 (Gauss distribution 1)\n")
                 f.write("   0.0000    0.0000    0.0000    !! stickiness parameter (for interactions only)\n")
                 f.write(" 2                      !! ASCII format file\n")
-                f.write(dat+"        !! Experimental data file\n")
-                f.write(dat.split(".dat")[0]+"        !! Output prefix name\n")
+                f.write(dat + "        !! Experimental data file\n")
+                f.write(dat.split(".dat")[0] + "        !! Output prefix name\n")
                 f.write("1                       !! Angular scale (1/2/3/4) as in GNOM\n")
                 f.write("1.0                     !! Exp. data portion to fit (from beginning)\n")
                 f.write("meth sb                 !! Minimization method sb - \"simple bounds\"\n")
@@ -119,35 +119,30 @@ def generate_lipmix_submission(l,d, dat):
                 f.write("y\n")
                 f.write("y\n")
 
-
-
-
                 f.close()
 
-
-       
-
-                os.system("lipmix <"+fn)
+                os.system("lipmix <" + fn)
                 os.chdir("../")
     os.chdir("../")
 
-#Arguments parser
-parser = argparse.ArgumentParser(description='Wrap-up script to generate multiple input files for LIPMIX with iterative number of layers. Example: python lipmix_multilayer_autofinder.py -i test_lipmix_MLV.dat -l 1 -d 1')
-parser.add_argument('-l','--layers', default=1, required=True,type=int, help="Total expected number of layers.")
-parser.add_argument('-d','--distr', default=1, required=True,type=int, help="Total expected number of distributions.")
-parser.add_argument('-i','--input', nargs='+',required=True, default="input.dat", type=str, help="Input dat file")
+
+# Arguments parser
+parser = argparse.ArgumentParser(description='Wrap-up script to generate multiple input files for LIPMIX with '
+                                             'iterative number of layers. Example: python '
+                                             'lipmix_multilayer_autofinder.py -i test_lipmix_MLV.dat -l 1 -d 1')
+parser.add_argument('-l', '--layers', default=1, required=True, type=int, help="Total expected number of layers.")
+parser.add_argument('-d', '--distr', default=1, required=True, type=int, help="Total expected number of distributions.")
+parser.add_argument('-i', '--input', nargs='+', required=True, default="input.dat", type=str, help="Input dat file")
 args = parser.parse_args()
 
-#Main part
+# Main part
 matched_files = []
 for file in args.input:
     if glob.escape(file) != file:
         matched_files.extend(glob.glob(file))
     else:
         matched_files.append(file)
-        
+
         for file in matched_files:
-            print (file)
+            print(file)
             generate_lipmix_submission(args.layers, args.distr, file)
-
-
--- lipmix_multilayer_autofinder_SLURM.py
+++ lipmix_multilayer_autofinder_SLURM.py
@@ -5,15 +5,15 @@ import itertools
 import argparse
 import shutil
 
-#=======================================================
-# Plese check options in "Cluster submission" section
+
+# =======================================================
+# Please check options in "Cluster submission" section
 # for the particular cluster in use. Example is written 
 # based on SLURM submission system.
-#=======================================================
+# =======================================================
 
 
-def generate_lipmix_submission(l,d, dat):
-
+def generate_lipmix_submission(l, d, dat):
     n_dist = d
     total_l = l
 
@@ -21,42 +21,39 @@ def generate_lipmix_submission(l,d, dat):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    
-
-
     os.chdir(directory)
-    
-    for dis in np.arange(1,n_dist+1):
 
-        list_l = list(np.arange(1,total_l+1))
+    for dis in np.arange(1, n_dist + 1):
+
+        list_l = list(np.arange(1, total_l + 1))
 
         tot_com = 0
 
-        for l in np.arange(1,total_l+1):
+        for l in np.arange(1, total_l + 1):
 
             perm = itertools.combinations(list_l, l)
 
             for p in perm:
-                tot_com = tot_com+1
-                fn = dat.split(".dat")[0]+"_n_dist_"+str(dis)+"_"+str(list(p)).replace(", ","_")+"_"+str(tot_com)
+                tot_com = tot_com + 1
+                fn = dat.split(".dat")[0] + "_n_dist_" + str(dis) + "_" + str(list(p)).replace(", ", "_") + "_" + str(
+                    tot_com)
 
                 directory = fn
                 if not os.path.exists(directory):
                     os.makedirs(directory)
 
                 os.chdir(directory)
-                shutil.copy2("../../"+dat, "../"+directory)
+                shutil.copy2("../../" + dat, "../" + directory)
 
-                f=open(fn,"w")
+                f = open(fn, "w")
 
-                
                 f.write("i\n")
                 f.write("!!!!!!!!!!!!!!!\n")
                 f.write("pro us\n")
                 f.write("Series titles\n")
                 f.write("Partial data titles\n")
                 f.write("EXPERIMENT\n")
-                f.write(str(dis+2)+"           !! Number of components\n")
+                f.write(str(dis + 2) + "           !! Number of components\n")
                 f.write("0.2000   !! System concentration\n")
                 f.write("DIFFUSE\n")
                 f.write("   0.50    0.0000    100.000    !! Volume fraction of the bilayer component\n")
@@ -68,14 +65,16 @@ def generate_lipmix_submission(l,d, dat):
                 f.write("   0.300     0.300     0.300     !! Width of peak3 position (negative density)\n")
                 f.write("   0.833     0.833     0.833     !! Amplitude ratio (Peak3/Peak1) (negative density)\n")
                 f.write("   0.05    0.01      0.20       !! Callie parameter\n")
-                
 
-                f.write("   "+str(len(p))+"        "+str(len(p))+"       "+str(len(p))+"       "+"!! Total Number of multilayer vesicles (alwayed FIXED)\n")
+                f.write("   " + str(len(p)) + "        " + str(len(p)) + "       " + str(
+                    len(p)) + "       " + "!! Total Number of multilayer vesicles (always FIXED)\n")
                 for pi in p:
-                    f.write("   "+str(pi)+"       "+str(pi)+"       "+str(pi)+"       "+"!! Number of layers for vesicle type"+str(pi)+"\n")
-                    print (f.write("   1.0      0.0       1.0       !! Weight contribution of vesicle type "+str(pi)+"\n"))
+                    f.write("   " + str(pi) + "       " + str(pi) + "       " + str(
+                        pi) + "       " + "!! Number of layers for vesicle type" + str(pi) + "\n")
+                    print(f.write(
+                        "   1.0      0.0       1.0       !! Weight contribution of vesicle type " + str(pi) + "\n"))
 
-                for dis in np.arange(1,dis+1):
+                for dis in np.arange(1, dis + 1):
                     f.write("    SPHERE\n")
                     f.write("   0.500    0.00100    1.0000    !! Volume fraction of the component (vesicle/micelle)\n")
                     f.write("   0.0000    0.0000    0.0000    !! Inner (core) radius of the sphere\n")
@@ -86,8 +85,6 @@ def generate_lipmix_submission(l,d, dat):
                     f.write("  86.9196   86.9196   86.9196    !! Hard-sphere radius (for interactions only)\n")
                     f.write("2                                !! Schulz distribution 2 (Gauss distribution 1)\n")
                     f.write("   0.0000    0.0000    0.0000    !! stickiness parameter (for interactions only)\n")
-
-
 
                 f.write("SPHERE\n")
                 f.write("   0.0050    0.0000    1.0000    !! Volume fraction of the component (vesicle/micelle)\n")
@@ -100,8 +97,8 @@ def generate_lipmix_submission(l,d, dat):
                 f.write("2                                !! Schulz distribution 2 (Gauss distribution 1)\n")
                 f.write("   0.0000    0.0000    0.0000    !! stickiness parameter (for interactions only)\n")
                 f.write(" 2                      !! ASCII format file\n")
-                f.write(dat+"        !! Experimental data file\n")
-                f.write(dat.split(".dat")[0]+"        !! Output prefix name\n")
+                f.write(dat + "        !! Experimental data file\n")
+                f.write(dat.split(".dat")[0] + "        !! Output prefix name\n")
                 f.write("1                       !! Angular scale (1/2/3/4) as in GNOM\n")
                 f.write("1.0                     !! Exp. data portion to fit (from beginning)\n")
                 f.write("meth sb                 !! Minimization method sb - \"simple bounds\"\n")
@@ -116,17 +113,13 @@ def generate_lipmix_submission(l,d, dat):
                 f.write("y\n")
                 f.write("y\n")
 
-
-
-
                 f.close()
 
-                #====================
+                # ====================
                 # Cluster submission.
-                #====================
+                # ====================
 
-
-                f2=open(fn+"_clust.sh","w")
+                f2 = open(fn + "_clust.sh", "w")
                 f2.write("#! /bin/bash\n")
                 f2.write("#SBATCH --export=ALL\n")
                 f2.write("#SBATCH --error=%x.stderrn")
@@ -135,37 +128,32 @@ def generate_lipmix_submission(l,d, dat):
                 f2.write("#SBATCH --mail-user=agruzinov@embl-hamburg.de\n")
                 f2.write("source /apps/prod/atsas/atsas-env latest\n")
                 f2.write("cd ${SLURM_SUBMIT_DIR}\n")
-                f2.write("lipmix < "+fn +"\n")
-                f2.close()  
-                os.system("sbatch "+fn+"_clust.sh")
+                f2.write("lipmix < " + fn + "\n")
+                f2.close()
+                os.system("sbatch " + fn + "_clust.sh")
 
-                #================================
+                # ================================
 
-
-       
-
-                
                 os.chdir("../")
     os.chdir("../")
 
 
-#Arguments parser
-parser = argparse.ArgumentParser(description='Wrap-up script to generate multiple input files for LIPMIX with iterative number of layers. Example: python lipmix_multilayer_autofinder.py -i test_lipmix_MLV.dat -l 1 -d 1')
-parser.add_argument('-l','--layers', default=1, required=True,type=int, help="Total expected number of layers.")
-parser.add_argument('-d','--distr', default=1, required=True,type=int, help="Total expected number of distributions.")
-parser.add_argument('-i','--input', nargs='+',required=True, default="input.dat", type=str, help="Input dat file")
+# Arguments parser
+parser = argparse.ArgumentParser(description='Wrap-up script to generate multiple input files for LIPMIX with '
+                                             'iterative number of layers. Example: ''python '
+                                             'lipmix_multilayer_autofinder.py -i test_lipmix_MLV.dat -l 1 -d 1')
+parser.add_argument('-l', '--layers', default=1, required=True, type=int, help="Total expected number of layers.")
+parser.add_argument('-d', '--distr', default=1, required=True, type=int, help="Total expected number of distributions.")
+parser.add_argument('-i', '--input', nargs='+', required=True, default="input.dat", type=str, help="Input dat file")
 args = parser.parse_args()
 
-#Main part
+# Main part
 matched_files = []
 for file in args.input:
     if glob.escape(file) != file:
         matched_files.extend(glob.glob(file))
     else:
         matched_files.append(file)
-        
         for file in matched_files:
-            print (file)
+            print(file)
             generate_lipmix_submission(args.layers, args.distr, file)
-
-            
